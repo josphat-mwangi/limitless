921b76ce3d58be2e10ce28cb1e26d7061c121e56
josphat mwangi <josphatwanjiruw@gmail.com>
2022-07-06 00:12:31 +0300

    update email

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -64,7 +64,7 @@ const Contact = () => {
                 
                     <div className="mt-4 text-[#23314C] text-center">
                         <h1 className="text-xl font-bold">Write to Us</h1>
-                        <p className="mt-4 text-[#23314C]">sam@gmail.com</p>
+                        <p className="mt-4 text-[#23314C]">Securilimitlessltd2021@gmail.com</p>
                         <div className="flex justify-center items-center">
                         <svg xmlns="http://www.w3.org/2000/svg" className="h-16 w-16" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z" />
