13a33532ef6b4ac22854589401391764fef37f00
josphat mwangi <josphatwanjiruw@gmail.com>
2023-01-15 00:08:55 +0300

    create data store to store wording and images

---
+++ frontend/utilities/dataStore.js
@@ -0,0 +1,203 @@
+export const sliderData = [
+    {
+      image: '/assets/m1.jpeg',
+      headline: 'Not sure about the cost?',
+      paragraph:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste obcaecati aperiam, repellendus voluptatibus quasi iusto quae dolores',
+     
+    },
+    {
+      image: '/assets/m2.jpeg',
+      headline: 'get creative with green energy.',
+      paragraph:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste obcaecati aperiam, repellendus voluptatibus quasi iusto quae dolores',
+      
+    },
+    {
+      image: '/assets/brandprotection.jpeg',
+      headline: 'get the job done right $ effeciently.',
+      paragraph:
+        'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Iste obcaecati aperiam, repellendus voluptatibus quasi iusto quae dolores',
+      
+    },
+];
+
+
+export const _items = [
+  {
+      
+      title: 'Efren Reyes',
+      image: 'assets/company1.png',
+      
+  },
+  {
+    
+      title: "Ronnie O'Sullivan",
+      image: 'assets/company2.png',
+      
+  },
+  {
+      
+      title: 'Shane Van Boening',
+      image: 'assets/company3.png',
+      
+  },
+  {
+     
+      title: 'Mike Sigel',
+      image: 'assets/company4.png',
+    
+  },
+  {
+      
+      title: 'Willie Mosconi',
+      image: 'assets/company5.png',
+      
+  },
+];
+
+
+export const HomeServiceItems =[
+  {
+      title:'Brand Protection',
+      explanation1: 'Brand protection is the process of protecting the intellectual property (IP) of companies and their associated brands against counterfeiters, copyright pirates, and infringers of other types of IP, such as patents, design rights, color mark and trade dress.',
+      explanation2: '',
+      url:'/services',
+  },
+  {
+      title:'Man Guarding',
+      explanation1: 'We are experienced in tailoring integrated security solution for the needs of diverse customer base',
+      explanation2: '',
+      url:'/services',
+  },
+  {
+      title:'Private investigations',
+      explanation1: 'Our Team of private investigators have had phenomenal results over the years. Get in touch and see what we can do for you',
+      explanation2: '',
+      url:'/services',
+  },
+  {
+      title:'Cash in Transit',
+      explanation1: 'Secure transportation of cash using high security vehicles, fully vetted and trained personnel and purpose-built technology to transport, protect, count and reconcile cash to customer records',
+      explanation2: '',
+      url:'/services',
+  },
+  {
+      title:'Security Consultancy',
+      explanation1: 'Limitles Security offers consultancy services to cooprates, undividuals in matters Security.',
+      explanation2: 'We assist our client but not limmited to formulating security policies, safety training, basic safety measures(both at home and workplaces) among other security issues consulted',
+      url:'/services',
+  },
+  {
+      title:'Canine Services',
+      explanation1: 'Our canine department include trained dogs in different discplines to server peculiar clientele need.',
+      explanation2: '',
+      url:'/services',
+  },
+  {
+      title: 'Business Risk Analysis',
+      img: '',
+      explanation1: 'Limitless Security aims at assisting our broad specturm of client in not only analysising but also the risk factors but goess further to mitigate and avert the solid risks.',
+      explanation2: 'Limitless Security also conducts due diliqence for investors and partners before committing.',
+      url:'/services',
+
+  },
+  {
+      title: 'Background Check',
+      img: '',
+      explanation1: "Limitless assits employers/investors in verifying the validity of someone's information.",
+      explanation2: 'Using reliable and official national and intel data sources to screen candidates, while complying to data and privacy protection.',
+      url:'/services',
+
+  },
+  {
+      title: 'Litigation Support',
+      img: '',
+      explanation1: 'Over the years, our Integrated Security has broadened its vision and grown to become an important resource to insurance carriers, insurance defense firms and the corporate business community seeking litigation support services for all phases of case preparation. Specializing in file review, witness location, obtaining written or recorded statements, asset location investigations, surveillance, and service of legal documents. We offer nationwide services.',
+      explanation2: '',
+      url:'/services',
+
+  },
+  
+
+]
+
+export const ServiceItems =[
+    {
+        title: 'Brand Protection',
+        img: '/assets/brandprotection.jpeg',
+        explanation1: 'This is done not only to protect the loss of revenue from a company but also to protect a company’s image, reputation and overall value. Fundamentally, brand protection prevents brand abuse.',
+        explanation2: 'Brand abuse can come in many forms, including, but not limited to:',
+        features:'Counterfeiting, Rogue websites, Copyright piracy, Trademark squatting, Patent theft, Social media impersonation, Parallel trading, Illegal import',
+        
+
+    },
+    {
+        title: 'Man Guarding',
+        img: 'assets/guard.png',
+        explanation1: 'The effective utilisation of trained, competent and compliant asset protection officers that add value not only to a site’s security program, but also to the client’s brand is key to any risk management plan. With off-site management support, electronic management tools and site specific SOP-based training, Limitless Guarding embodies these skills and values.',
+        explanation2: 'Guarding services: ',
+        features:'Access and Egress control, Security Guards, System Checkers, Searching, Intelligent Patrolling, Concierge Services, Reception Services, Control Room Services, Escorting, Weighbridge Operators'
+      
+
+    },
+    {
+        title: 'Cash in Transit',
+        img: 'assets/cot-removebg-preview.png',
+        explanation1: 'Our end-to-end approach to cash handling, allows us to manage your cash throughout the entire cash cycle – from your deposit in the cash vault to the bank, including the Cash-in-Transit (CIT) logistics, providing you with an immediate risk transfer.',
+        explanation2: "With our full end-to-end cash management contract, we assume full responsibility of managing the CIT service on your behalf.",
+        features:"",
+
+    },
+    {
+        title: 'Private Investigation',
+        img: 'assets/private investigation.png',
+        explanation1: 'Private investigators are much more than the shady individuals you see in movies or TV shows. Instead, they’re agents who can conduct professional investigation work, whether it be corporate fraud, or evidence gathering for an important legal case. ',
+        explanation2: 'Hiring us as  private investigator can help you for a variety of reasons, such as giving you fresh eyes on a situation, or providing additional security intel.',
+        features:"",
+
+    },
+    {
+        title: 'Security Consultancy',
+        img: 'assets/sconsultancy.jpeg',
+        explanation1: 'Security consulting encompasses the full suite of services that Limitless Security offers to clients. We provide you with the knowledge, tools and capability to make better security decisions across all your assets, be it one property or hundreds.',
+        explanation2: 'Having access to a security expert and confidential adviser is just one of the benefits of engaging a security consultant',
+        features:"",
+    },
+    {
+        title: 'Canine Services',
+        img: 'assets/k-9services3.jpg',
+        explanation1: 'Our dog operations include sent Detection, tracking Protection, crowd management and Security team',
+        explanation2: 'Our scent detection dogs are imprinted in either: Narcotics or Explosive',
+        features:"",
+
+    },
+    {
+        title: 'Business Risk Analysis',
+        img: 'assets/risk.jpeg',
+        explanation1: 'Our knowledgeable and experienced team offer risk management consulting across a range of key business functions. Our services include IT risk advisory and assurance, determining governance compliance, undertaking forensic investigations, data analytics interrogation and establishing SOX compliance. This allows us to determine any potential risks to your business and the maturity of them.',
+        explanation2: '',
+        features:"",
+
+    },
+    {
+        title: 'Background Check',
+        img: 'assets/backgroundcheck.jpeg',
+        explanation1: 'Our nationwide team is professionally trained and fully equipped to perform background checks for employers. We also offer background checks for private citizens if you need to verify someones identity, check a persons criminal record, employment information, and education. Contact us now to request more information.',
+        explanation2: '',
+        features:"",
+
+    },
+    {
+        title: 'Litigation Support',
+        img: 'assets/litigation.jpeg',
+        explanation1: 'Experienced investigators deliver case-changing insights to support the entire litigation lifecycle.',
+        explanation2: 'Go beyond evidence to insight: our experienced investigators can collect, analyze and present any form of evidence as experts to deliver case-changing insights in litigation, arbitration or disputes.',
+        features:"",
+
+    },
+    
+    
+    
+]
+
