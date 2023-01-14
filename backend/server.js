2291c70875327643f81ed0adbae0fe997dc05efa
josphat mwangi <josphatwanjiruw@gmail.com>
2023-01-15 00:17:39 +0300

    create backend for emailing and other files

--- backend/server.js
+++ backend/server.js
@@ -11,11 +11,13 @@ app.use(bodyparser.json())
 app.use(bodyparser.urlencoded({extended: true}))
 app.use(express.json())
 
-
+app.get("/", (req, res)=>{
+    res.send("Its working")
+})
 app.use(emailRoutes)
 
 
 
 app.listen(process.env.PORT || 5000, ()=>{
-    console.log('server is running')
+    console.log(`server is running ${process.env.PORT}`)
 })
